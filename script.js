f8301a0f1cdfc6ed2cbba2fef6c40e4622bfbcd0
krit <kuwarkritvardhan@gmail.com>
2025-01-05 01:20:49 +0530

    Updated

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 (function() {
-  // Initialize EmailJS with the public key
   emailjs.init("h9CaHHphd5WNUeG1P");
 })();
 
-// Add event listener for form submission
 function sendEmail(event) {
-  event.preventDefault(); // Prevent default form submission
-  // if (!window.env) { // Ensure 'env' variable is set, or remove this check
-  //     console.error('Environment variables not loaded.');
-  //     return;
-  // }
+  event.preventDefault();
   emailjs.sendForm('service_pogs0ro', 'template_r3qxzws', event.target)
       .then(() => {
-          // Show success message
           const successMessage = document.getElementById('success-message');
-          successMessage.style.display = 'block'; // Show the success message
-
-          // Reset the form fields
+          successMessage.style.display = 'block';
           event.target.reset();
-
-          // Hide the success message after 1 second
           setTimeout(() => {
-              successMessage.style.display = 'none'; // Hide the success message
-          }, 1000); // 1 second
+              successMessage.style.display = 'none'; 
+          }, 1000);
       }, (error) => {
           console.log('FAILED...', error);
       });
 }
 
+
 // Attach the submit event listener directly to the form
 document.getElementById('contact-form').addEventListener('submit', sendEmail);
+
+function notification(event){
+  event.preventDefault();
+  successMessage.style.display = 'block';
+  event.target.reset();
+  setTimeout(() => {
+      successMessage.style.display = 'none';
+  }, 2000);
+}
+
+document.getElementById('contact-form').addEventListener('submit', notification);
 
 // Loader Animation
 function loaderAnimation() {
