a783aae8dfebf90700fc2a1c3acf98f2991e1c6e
krit <kuwarkritvardhan@gmail.com>
2025-01-04 15:20:41 +0530

    update

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function loaderAnimation() {
   var loader = document.querySelector("#loader")
   setTimeout(function () {
       loader.style.top = "-100%"
-  }, 4000)
+  }, 700)
 }
 
 loaderAnimation()
