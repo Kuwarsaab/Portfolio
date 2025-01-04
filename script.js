b4ceb64f74302662e1d34aa3f91a96fa60cd8f32
krit <kuwarkritvardhan@gmail.com>
2025-01-04 12:13:46 +0530

    New Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    sendEmail(e.target);
+  });
+
+import emailjs from 'emailjs-com';
+
+const sendEmail = (form) => {
+  emailjs.sendForm('service_pogs0ro', 'template_r3qxzws', form, 'h9CaHHphd5WNUeG1P')
+    .then((result) => {
+      console.log('Email successfully sent!', result.text);
+    })
+    .catch((error) => {
+      console.error('Error sending email:', error);
+    });
+};
